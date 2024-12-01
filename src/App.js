9a9d061c6326c166b51fc83d987b81337a6c89ca
galiData <gal@corvexanalytics.com>
2024-12-02 01:27:32 +0200

    GV: media load time 4 minutes

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ const MedicalTranscription = () => {
     
     try {
       let attempts = 0;
-      const maxAttempts = 60; // 2 minute total (2 second intervals)
+      const maxAttempts = 120; // 4 minute total (2 second intervals)
       const pollInterval = 2000;
 
       const pollForTranscription = async () => {
